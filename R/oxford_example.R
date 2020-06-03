9da6d7dfbad7f1c45f500083d561ef9f5a5155d0
Hussein Mahfouz <husseinmahfouz93@gmail.com>
2020-06-03 20:50:35 +0100

    growth function in 5_prioritizing_segments

--- R/oxford_example.R
+++ R/oxford_example.R
@@ -8,6 +8,7 @@ library(lwgeom)
 city_names <- read_csv('../data-raw/Middle_Layer_Super_Output_Area__2011__to_Major_Towns_and_Cities__December_2015__Lookup_in_England_and_Wales.csv') 
 # change column name
 city_names <- city_names %>% rename(city = TCITY15NM)
+
 # flow data from the 2011 census https://www.nomisweb.co.uk/census/2011/bulk/rOD1
 flows <- read_csv('../data-raw/flow_data.csv')
 
@@ -31,9 +32,12 @@ flows_internal <- function(name) {
   return(x)
 }
 
-# use function to get flows between all MSOAs in Oxford
+# use function to get flows between all MSOAs in Oxford 
 flows_ox <- flows_internal("Oxford") %>% select(1:3) %>% 
   rename(potential_demand = `All categories: Method of travel to work`)   # TEMPORARY!!
+
+#clear env
+rm(flows)
 
 # download spatial data locally and read it
 msoa_boundaries <- st_read('../data-raw/MSOA_2011_Boundaries/Middle_Layer_Super_Output_Areas__December_2011__Boundaries.shp') %>%
@@ -55,7 +59,9 @@ spatial_ox <-
   st_as_sf() %>% lwgeom::st_make_valid() %>%
   mutate(centroid = st_centroid(geometry))  # we will need centroids to calculate distance matrix
 
+#clear env
 rm(msoa_boundaries)
+
 # Remove border geometry and set centroids as geometry. This does not overwrite 'spatial_ox' sf
 msoa_centroids <- 
   st_drop_geometry(spatial_ox) %>% 
@@ -90,7 +96,7 @@ st_snap_points = function(x, y, max_dist = 1000) {
 }
 
 msoa_centroids_snapped <- 
-  st_snap_points(msoa_centroids, roads, max_dist = 500) %>%   # if dist to nearest road > max dist, point is unchanged
+  st_snap_points(msoa_centroids, roads, max_dist = 1000) %>%   # if dist to nearest road > max dist, point is unchanged
   st_as_sf() %>%   # convert to get it in a dataframe
   bind_cols(msoa_centroids) %>%  # bind with msoa centroids df to ge MSOA IDs
   select(-c(centroid)) %>% # drop old geometry
@@ -103,8 +109,9 @@ plot(st_geometry(msoa_centroids_snapped), add = TRUE, col = 'green')
 
 # check that bind_cols() was correct. Plot individual points:
 plot(st_geometry(roads))
-plot(st_geometry(msoa_centroids$centroid[5]), add = TRUE, col = 'red')
-plot(st_geometry(msoa_centroids_snapped$centroid[5]), add = TRUE, col = 'green')
+plot(st_geometry(msoa_centroids$centroid[1]), add = TRUE, col = 'red')
+plot(st_geometry(msoa_centroids_snapped$centroid[1]), add = TRUE, col = 'green')
+
 #clear environment
 rm(roads)
 ########################
@@ -124,7 +131,9 @@ split_lon_lat <- function(x, names = c("lon","lat")) {
 
 
 # get lon and lat as seperate columns
-lon_lat <-  msoa_centroids %>% split_lon_lat() %>% select(-c(MSOA11NM)) 
+#lon_lat <-  msoa_centroids %>% split_lon_lat() %>% select(-c(MSOA11NM)) 
+lon_lat <-  msoa_centroids_snapped %>% split_lon_lat() %>% select(-c(MSOA11NM)) 
+
 
 ############
 # ROUTING USING DODGR
@@ -138,15 +147,17 @@ lon_lat <-  msoa_centroids %>% split_lon_lat() %>% select(-c(MSOA11NM))
 # Buffers can be added using expand in `dodgr_streetnet("oxford uk", expand = 0.05)`. the problem with this 
 # is that it gets a rectangular bb. I want something more compact (i.e buffer around city boundary). This is 
 # done to deal with RAM limitations
-##########
 
 # get compact bounding box with buffer
 
 # check functions.R for documentation. 
-bb_ox <- bb_buffer(city= "oxford uk", buffer= 1000)
+#bb_ox <- bb_buffer(city= "oxford uk", buffer= 1000)
+#streetnet <- dodgr_streetnet (bbox = bb_ox) 
+##########
 
-streetnet <- dodgr_streetnet (bbox = bb_ox) 
+streetnet <- dodgr_streetnet("oxford uk", expand = 0.05)
 graph <- weight_streetnet(streetnet, wt_profile = "bicycle")
+
 
 # remove flows where ORIGIN = DESTINATION
 od_flow <- flows_ox %>% filter(`Area of residence` != `Area of workplace`)
@@ -192,6 +203,9 @@ graph$flow <- graph_undir$flow
 
 graph_f <- graph_f [graph_f$flow > 0, ]
 dodgr_flowmap (graph_f, linescale = 5)
+
+
+
 
 
 
